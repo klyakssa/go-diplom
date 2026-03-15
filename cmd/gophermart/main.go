4bdd3213376fecf649b23164ca68a66ee6683705
Denis <denis.spb.2004@gmail.com>
2026-03-15 23:29:37 +0300

    refactor(auth): :recycle: add to main.go code to run app

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,3 +1,12 @@
 package main
 
-func main() {}
+import (
+	"github.com/klyakssa/go-diplom.git/internal/app"
+	"github.com/klyakssa/go-diplom.git/internal/config"
+)
+
+func main() {
+	config := config.InitConfiguration()
+
+	app.Run(config)
+}
