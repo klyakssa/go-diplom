cc5c4c51fab197a6813ea86ff3bc56a6c01c87f1
Denis <denis.spb.2004@gmail.com>
2026-03-18 22:01:48 +0300

    fix(orders): :bug: fix #7

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"github.com/klyakssa/go-diplom.git/internal/app"
 	"github.com/klyakssa/go-diplom.git/internal/config"
+	"github.com/shopspring/decimal"
 )
 
 func main() {
 	config := config.InitConfiguration()
+	decimal.MarshalJSONWithoutQuotes = true
 
 	app.Run(config)
 }
